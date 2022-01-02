75746d3c16c428ffc2103b773f4ffe929edc7833
HangTo <tominhhang1996@gmail.com>
2022-01-02 20:40:27 +0700

    Implement AlertService

--- src/service/alert.service.js
+++ src/service/alert.service.js
@@ -29,3 +29,26 @@ function onAlert(id = defaultId) {
 function success(message, options) {
     alert({...options, type: alertType.success, message });
 }
+
+function error(message, options) {
+    alert({...options, type: alertType.error, message });
+}
+
+function info(message, options) {
+    alert({...options, type: alertType.info, message });
+}
+
+function warn(message, options) {
+    alert({...options, type: alertType.warning, message });
+}
+
+// core alert method
+function alert(alert_obj) {
+    alert_obj.id = alert_obj || defaultId;
+    alertSubject.next();
+}
+
+// clear alerts
+function clear(id = defaultId) {
+    alertSubject.next({ id });
+}
