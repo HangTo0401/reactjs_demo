21602b563ff237321b5209389539547f84d2f4c5
HangTo <tominhhang1996@gmail.com>
2022-01-16 08:45:08 +0700

    Implement ShoppingCartMessage

--- src/components/ShoppingCartMessage.js
+++ src/components/ShoppingCartMessage.js
@@ -8,7 +8,11 @@ class ShoppingCartMessage extends Component {
 
     render() {
         return(
-            <div></div>
+            <div>
+                <h3>
+                    <span className="badge amber darken-2">Mua Hàng Thành Công !</span>
+                </h3>
+            </div>
         );
 
     };
