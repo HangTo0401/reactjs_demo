76fd4e437609dc22a069319152f4202a31db6a60
HangTo <tominhhang1996@gmail.com>
2022-02-12 11:54:54 +0700

    Implement NotFoundPage

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,7 @@ import {
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
+import NotFound from './NotFound';
 
 const MenuLink = ({ label, to, activeOnlyWhenExact }) => {
     var active = 'active';
@@ -42,6 +43,7 @@ export class Header extends Component {
                         <Route path='/' element={<Home/>}/>
                         <Route path='/about' element={<About/>}/>
                         <Route path='/contact' element={<Contact/>}/>
+                        <Route path='*' element={<NotFound/>}/>
                     </Routes>
                 </Router>
             </StyledHeader>
---
+++ src/components/NotFound.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+    render() {
+        return (
+            <div style={{color: "#c5fff9"}}>
+                <h3>404 -Page Not Found</h3>
+            </div>
+        )
+    }
+}
+
+export default NotFound
