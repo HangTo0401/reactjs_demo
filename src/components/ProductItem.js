74a520d3b83ab2c6bf25017fe926b08ba7da6fc1
HangTo <tominhhang1996@gmail.com>
2022-02-15 18:34:49 +0700

    Implement ProductItem

---
+++ src/components/ProductItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ProductItem extends Component {
+    render() {
+        return (
+            <div>Đây là trang chi tiết sản phẩm</div>
+        );
+    };
+}
+
+export default ProductItem;
