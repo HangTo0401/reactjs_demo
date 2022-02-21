02b40449b60db76c1d625a3a083042cffbbd6477
HangTo <tominhhang1996@gmail.com>
2022-02-21 19:01:00 +0700

    Change Menu.js

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,9 +1,20 @@
+import React, { Component } from 'react';
+import { Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, } from 'react-router-dom';
 class Menu extends Component {
     render() {
         return(
-            <div>
-                
-            </div>
+            <Router>
+                <div className='container-fluid'>
+                    <div className='row'>
+                        <Nav className='navbar-default'>
+                            <a className="navbar-brand">Call API</a>
+                            <a className="navbar-brand">Trang Chủ</a>
+                            <a className="navbar-brand">Quản lí sản phẩm</a>
+                        </Nav>
+                    </div>
+                </div>
+            </Router>
         )
     }
 }
