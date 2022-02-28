76cbecfdb5a415398d06ad710788fad9026f43c7
HangTo <tominhhang1996@gmail.com>
2022-02-28 18:37:32 +0700

    Implement axios

--- src/components/ProductListPage/ProductListPage.js
+++ src/components/ProductListPage/ProductListPage.js
@@ -2,7 +2,31 @@ import React, { Component } from 'react'
 import ProductItem from '../ProductItem/ProductItem';
 import ProductList from './../ProductList/ProductList'
 import { connect } from 'react-redux';
+import axios from 'axios';
 class ProductListPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: []
+        };
+    }
+
+    componentDidMount() {
+        return axios({
+            method: 'get',
+            url: 'http://localhost:3000/products',
+            data: null
+        })
+        .then(res => {
+            if (res.status === 200) {
+                this.setState({
+                    products: res.data
+                })
+            }
+        })
+        .catch(res => {console.log(res)});
+    };
+
     showProducts(products) {
         var result = null
         if (products.length > 0) {
@@ -16,7 +40,8 @@ class ProductListPage extends Component {
     }
     
     render() {
-        var { products } = this.props
+        var { products } = this.state
+
         return(
             <div className="container">
                 <div className="row">
