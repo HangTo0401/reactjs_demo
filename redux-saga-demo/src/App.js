27e395ae418591cac183be300841fa65488d251e
HangTo <tominhhang1996@gmail.com>
2022-03-01 23:20:33 +0700

    Create project redux-saga-demo

---
+++ redux-saga-demo/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Button from '@material-ui/core/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello Redux Saga</h2>
+      <Button variant='contained' color='primary'>Lưu</Button>
+    </div>
+  );
+}
+
+export default App;
