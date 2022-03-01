1e0661152367654afa716438b405f281e6d93f67
HangTo <tominhhang1996@gmail.com>
2022-03-01 08:39:05 +0700

    Add commons folder

---
+++ redux-saga-demo/src/commons/theme/index.js
@@ -0,0 +1,29 @@
+import { createTheme } from '@material-ui/core/styles';
+
+// All the following keys are optional.
+// We try our best to provide a great default value.
+const theme = createTheme({
+    palette: {
+      primary: "#b71c1c",
+      secondary: "#283593",
+      error: "#e53935",
+      // Used by `getContrastText()` to maximize the contrast between the background and
+      // the text.
+      contrastThreshold: 3,
+      // Used to shift a color's luminance by approximately
+      // two indexes within its tonal palette.
+      // E.g., shift from Red 500 to Red 300 or Red 700.
+      tonalOffset: 0.2,
+    },
+    typography: {
+      fontFamily: "Roboto"
+    },
+    shape: {
+      textColor: "#FFFFFF",
+      borderColor: "#CCCCCC",
+      backgroundColor: "#651fff",
+      borderRadius: "4px"
+    }
+});
+
+export default theme;
