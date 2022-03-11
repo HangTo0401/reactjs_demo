930099b516980442c7994bb501c888df73c73abc
HangTo <tominhhang1996@gmail.com>
2022-03-11 11:42:46 +0700

    Implement filter tasks based on status

--- redux-saga-demo/src/containers/Taskboard/index.js
+++ redux-saga-demo/src/containers/Taskboard/index.js
@@ -7,15 +7,49 @@ import Grid from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import { STATUS } from '../../constants/index';
+
+const listTasks = [
+  {
+    id: 0,
+    title: "Read book",
+    description: "Read Material UI book",
+    status: 0
+  },
+  {
+    id: 1,
+    title: "Play games",
+    description: "Play doraemon games",
+    status: 2
+  },
+  {
+    id: 2,
+    title: "Do homework",
+    description: "Do homework",
+    status: 1
+  }
+];
 class Taskboard extends Component {
   renderBoard() {
+    const { classes } = this.props
     let xHtml = null
     xHtml = (
       <Grid container spacing={2}>
         {
           STATUS.map((status, index) => {
+            const filteredTasks = listTasks.filter(task => task.status === status.value)
             return (
-              <Grid item md={4} xs={12} key={index}>{status?.label}</Grid>
+              <Grid item md={4} xs={12} key={status?.value}>
+                <div className={classes.status}>{status?.label}</div>
+                <div className={classes.wrapperListTasks}>
+                  {
+                    filteredTasks.map((task, index) => {
+                      return (
+                        <h1>{task.title}</h1>
+                      );
+                    })
+                  }  
+                </div>
+              </Grid>
             );
           })
         }
