ba0a95349f731c15d1407634249ee6197b51176d
HangTo <tominhhang1996@gmail.com>
2022-03-12 11:55:38 +0700

    Style for each task

--- redux-saga-demo/src/containers/Taskboard/index.js
+++ redux-saga-demo/src/containers/Taskboard/index.js
@@ -4,7 +4,11 @@ import React, { Component } from 'react';
 
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
-import { withStyles } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import Box from '@material-ui/core/Box';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import { Typography, withStyles } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import { STATUS } from '../../constants/index';
 
@@ -39,12 +43,27 @@ class Taskboard extends Component {
             const filteredTasks = listTasks.filter(task => task.status === status.value)
             return (
               <Grid item md={4} xs={12} key={status?.value}>
-                <div className={classes.status}>{status?.label}</div>
+                <Box mt={1} mb={1}>
+                  <div className={classes.status}>{status?.label}</div>
+                </Box>
                 <div className={classes.wrapperListTasks}>
                   {
-                    filteredTasks.map((task, index) => {
+                    filteredTasks.map((task) => {
+                      const { title } = task
                       return (
-                        <h1>{task.title}</h1>
+                        <Card key={task.id} className={classes.card}>
+                          <CardContent>
+                            <Grid container justifyContent='space-between'>
+                              <Grid item md={8}>
+                                <Typography component='h2'>{title}</Typography>  
+                              </Grid>
+                              <Grid item md={4}>{status.label}</Grid>
+                            </Grid>
+                          </CardContent>
+                          <CardActions>
+                            <Button size="small"></Button>
+                          </CardActions>
+                        </Card>
                       );
                     })
                   }  
