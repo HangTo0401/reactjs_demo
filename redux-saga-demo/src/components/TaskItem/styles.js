d33901f95b5f8c56aa5c4da90c133e651f45814f
HangTo <tominhhang1996@gmail.com>
2022-03-13 12:26:18 +0700

    Implement TaskList

---
+++ redux-saga-demo/src/components/TaskItem/styles.js
@@ -0,0 +1,10 @@
+// Viết CSS in JS
+const styles = (theme) => ({
+  cardActions: {
+    display: "flex",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+});
+
+export default styles;
