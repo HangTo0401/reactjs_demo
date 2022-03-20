aec5c726367745278d03c82c8dcfda5ceb06203a
HangTo <tominhhang1996@gmail.com>
2022-03-20 23:02:42 +0700

    Implement Redux-saga

--- redux-saga-demo/src/redux/configureStore.js
+++ redux-saga-demo/src/redux/configureStore.js
@@ -1,6 +1,8 @@
 import { createStore, compose, applyMiddleware } from "redux"
 import rootReducer from "../reducers";
 import thunk from 'redux-thunk';
+import createSagaMiddleware from '@redux-saga/core';
+import rootSaga from './../sagas/index';
 
 // Tạo Store tích hợp middleware và devtools
 const composeEnhancers = process.env.NODE_ENV !== 'production' 
@@ -9,12 +11,15 @@ const composeEnhancers = process.env.NODE_ENV !== 'production'
     shouldHotReload: false
 }) : compose;
 
+const sagaMiddleware = createSagaMiddleware();
+
 const configureStore = () => {
-    const middlewares = [thunk]
+    const middlewares = [thunk, sagaMiddleware]
     const enhancers = [
         applyMiddleware(...middlewares)
     ]
     const store = createStore(rootReducer, composeEnhancers(...enhancers));
+    sagaMiddleware.run(rootSaga);
     return store;
 }
 
---
+++ redux-saga-demo/src/sagas/index.js
@@ -0,0 +1,8 @@
+
+
+// Process dùng để lắng nghe actions đăng ký
+function* rootSaga(){
+    console.log('This is root saga')
+}
+
+export default rootSaga;
