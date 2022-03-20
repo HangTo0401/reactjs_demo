4268a1cc5368cb5d87c355f62e5217fbeb68e583
HangTo <tominhhang1996@gmail.com>
2022-03-20 20:55:48 +0700

    Add comments

--- redux-saga-demo/src/generators.js
+++ redux-saga-demo/src/generators.js
@@ -1,8 +1,12 @@
 console.log('Leanring Generator function')
 
 function* generatorsFunction() {
-    yield 2019
-    return "abc"
+    // câu lệnh 1
+    yield 2019 // dừng tại đây, done là false do vẫn còn giá trị trả về
+
+    // chạy lại vị trí đã dừng
+    // câu lệnh 2
+    return "abc" // sẽ kết thúc luôn generator functions và câu gọi next tiếp theo sẽ không còn thực thi
 }
 
 const result = generatorsFunction();
