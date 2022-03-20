49c39a2dea8527be593d93cf67e6bc9c5c7adef8
HangTo <tominhhang1996@gmail.com>
2022-03-20 17:38:51 +0700

    Add Store and Reducer

--- redux-saga-demo/src/containers/App/index.js
+++ redux-saga-demo/src/containers/App/index.js
@@ -6,13 +6,19 @@ import theme from './../../commons/theme';
 
 import { withStyles } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
+import { Provider } from 'react-redux';
+import configureStore from '../../redux/configureStore';
+
+const store = configureStore();
 class App extends Component {
   render() {
     const { classes } = this.props
     return (
-      <ThemeProvider theme={theme}>
-        <Taskboard/>  
-      </ThemeProvider>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <Taskboard/>  
+        </ThemeProvider>
+      </Provider>
     );
   }
 }
