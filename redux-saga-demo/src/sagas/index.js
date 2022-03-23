23404d0e40081f1891e1d8a17c1f26622be50bf4
HangTo <tominhhang1996@gmail.com>
2022-03-23 15:03:59 +0700

    Update index.js

--- redux-saga-demo/src/sagas/index.js
+++ redux-saga-demo/src/sagas/index.js
@@ -1,5 +1,6 @@
-import { fork } from 'redux-saga/effects';
+import { fork, take, call } from 'redux-saga/effects';
 import * as taskActionsType from './../constants/taskActionsType';
+import { getListTasks } from './../apis/taskApi';
 
 // Process dùng để lắng nghe actions đăng ký
 function* rootSaga(){
@@ -9,9 +10,13 @@ function* rootSaga(){
 }
 
 function* watchFetchListTasksActions(){
-    yield take(taskActionsType.FETCH_TASKS)
+    yield take(taskActionsType.FETCH_TASKS);
     // Đoạn code từ đây trở về sẽ bị block cho đến chỉ khi ta dispatch action FETCH_TASKS, thì đoạn code dưới mới chạy
     console.log('Watch fetch list tasks Actions')
+    const response = yield call(getListTasks);
+    // Block cho đến chỉ khi ta call xong API, thì đoạn code dưới mới chạy
+    console.log('Print response: ')
+    console.log(response)
 }
 
 function* watchCreateTaskActions(){
