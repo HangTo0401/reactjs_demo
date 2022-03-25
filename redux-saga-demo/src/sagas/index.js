fa415d62530f60325a535ca0e81c3633b223a22a
HangTo <tominhhang1996@gmail.com>
2022-03-25 14:40:59 +0700

    Commit index.js

--- redux-saga-demo/src/sagas/index.js
+++ redux-saga-demo/src/sagas/index.js
@@ -1,8 +1,18 @@
-
+import { fork } from 'redux-saga/effects';
 
 // Process dùng để lắng nghe actions đăng ký
 function* rootSaga(){
     console.log('This is root saga')
+    yield fork(watchFetchListTasksActions);// watchFetchListTasksActions is generator function
+    yield fork(watchCreateTaskActions);// watchCreateTaskActions is generator function, run parallel with watchFetchListTasksActions
+}
+
+function* watchFetchListTasksActions(){
+    console.log('Watch fetch list tasks Actions')
+}
+
+function* watchCreateTaskActions(){
+    console.log('Watch create task Actions')
 }
 
 export default rootSaga;
