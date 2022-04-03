18d0a8aa9466b2f879fcd79abf747ab17d9eb470
HangTo <tominhhang1996@gmail.com>
2022-04-03 16:58:53 +0700

    Implement AdminLayoutRoute

--- redux-saga-demo/src/containers/App/index.js
+++ redux-saga-demo/src/containers/App/index.js
@@ -5,26 +5,42 @@ import Taskboard from '../Taskboard';
 import CustomModal from '../../components/CustomModal/index';
 import GlobalLoading from '../../components/GlobalLoading/index';
 import theme from './../../commons/theme';
-
+import { ROUTES } from '../../constants/index';
 import { withStyles } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import { Provider } from 'react-redux';
 import configureStore from '../../redux/configureStore';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { BrowserRouter, Router } from 'react-router-dom';
+import AdminLayoutRoute from '../../commons/layout/AdminLayoutRoute/index';
 
 const store = configureStore();
 class App extends Component {
+  renderRoutes() {
+    let xHtml = null
+    xHtml = ROUTES.map((route, index) => {
+      return <AdminLayoutRoute key={index} route={route} />
+    })
+
+    return xHtml;
+  }
+
   render() {
     const { classes } = this.props
     return (
       <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <ToastContainer/>
-          <GlobalLoading/>
-          <CustomModal/>
-          <Taskboard/>
-        </ThemeProvider>
+        <BrowserRouter>
+          <ThemeProvider theme={theme}>
+            <ToastContainer/>
+            <GlobalLoading/>
+            <CustomModal/>
+            {/* <Taskboard/> */}
+            <Router>
+              {this.renderRoutes()}
+            </Router>
+          </ThemeProvider>
+        </BrowserRouter>
       </Provider>
     );
   }
