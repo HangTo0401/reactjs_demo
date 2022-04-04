319e0ffb419346558219e32fde5bf542eded6384
HangTo <tominhhang1996@gmail.com>
2022-04-04 20:54:50 +0700

    Style for Sidebar

--- redux-saga-demo/src/components/Dashboard/Sidebar/styles.js
+++ redux-saga-demo/src/components/Dashboard/Sidebar/styles.js
@@ -1,6 +1,20 @@
-// Viết CSS in JS
-const styles = () => ({
-    
-});
+const styles = theme => ({
+    drawerPaper: {
+      width: 240,
+      maxWidth: 240,
+      zIndex: 10,
+      height: '100%',
+      position: 'relative',
+    },
+    menuLink: {
+      textDecoration: 'none',
+      color: theme.color.defaultTextColor,
+    },
+    menuLinkActive: {
+      '&>div': {
+        backgroundColor: theme.color.hover,
+      },
+    },
+  });
   
-export default styles;
+  export default styles;
