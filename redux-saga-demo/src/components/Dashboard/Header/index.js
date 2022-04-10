703afec1b58efefcd123fdbd78ef6e2c02697a5f
HangTo <tominhhang1996@gmail.com>
2022-04-10 21:12:32 +0700

    Add Header.js

--- redux-saga-demo/src/components/Dashboard/Header/index.js
+++ redux-saga-demo/src/components/Dashboard/Header/index.js
@@ -1,10 +1,111 @@
 import React, { Component } from "react";
+import { withStyles } from '@material-ui/core';
+import styles from './styles.js';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuIcon from '@material-ui/icons/Menu';
 
+const menuId = 'primary-search-account-menu';
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      anchorEl: null,
+    };
+  }
+
+  handleProfileMenuOpen = e => {
+    this.setState({
+      anchorEl: e.currentTarget,
+    });
+  };
+
+  handleMenuClose = () => {
+    this.setState({
+      anchorEl: null,
+    });
+  };
+
+  renderMenu = () => {
+    const { anchorEl } = this.state;
+    const isMenuOpen = Boolean(anchorEl);
+    return (
+      <Menu
+        anchorEl={anchorEl}
+        anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+        id={menuId}
+        keepMounted
+        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+        open={isMenuOpen}
+        onClose={this.handleMenuClose}
+      >
+        <MenuItem onClick={this.handleMenuClose}>Logout</MenuItem>
+      </Menu>
+    );
+  };
+
+  handleToggleSidebar = () => {
+    const { showSidebar, onToggleSidebar } = this.props;
+    if (onToggleSidebar) {
+      onToggleSidebar(!showSidebar);
+    }
+  };
+  
   render() {
-    return <div>Header</div>;
+    const { classes } = this.props
+
+    return (
+      <div className={classes.grow}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={this.handleToggleSidebar}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography className={classes.title} variant="h6" noWrap>
+              {name}
+            </Typography>
+            <div className={classes.grow} />
+            <div className={classes.sectionDesktop}>
+              <IconButton
+                edge="end"
+                aria-label="Account of current user"
+                aria-controls={menuId}
+                aria-haspopup="true"
+                onClick={this.handleProfileMenuOpen}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+            </div>
+          </Toolbar>
+        </AppBar>
+        {this.renderMenu()}
+      </div>
+    
+    );
   }
 }
+
+Header.propTypes = {
+  classes: PropTypes.object,
+  name: PropTypes.string,
+  showSidebar: PropTypes.bool,
+  onToggleSidebar: PropTypes.func,
+};
 
 const mapStateToProps = state => {
     return {
