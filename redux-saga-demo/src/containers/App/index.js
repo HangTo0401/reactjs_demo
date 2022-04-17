c06d903619e7e6e9d0629399b44818b5ab2e1265
HangTo <tominhhang1996@gmail.com>
2022-04-17 21:15:56 +0700

    Change App.js

--- redux-saga-demo/src/containers/App/index.js
+++ redux-saga-demo/src/containers/App/index.js
@@ -1,44 +1,50 @@
 import './../../App.css';
 import styles from './styles.js';
 import React, { Component } from 'react';
-import Taskboard from '../Taskboard';
 import CustomModal from '../../components/CustomModal/index';
 import GlobalLoading from '../../components/GlobalLoading/index';
 import theme from './../../commons/theme';
-import { ROUTES } from '../../constants/index';
+import { ADMIN_ROUTES } from '../../constants/index';
 import { withStyles } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/styles';
 import { Provider } from 'react-redux';
 import configureStore from '../../redux/configureStore';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import { BrowserRouter, Router } from 'react-router-dom';
-import AdminLayoutRoute from '../../commons/layout/AdminLayoutRoute/index';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import AdminLayoutRoute from './../../commons/layout/AdminLayoutRoute/index';
 
 const store = configureStore();
 class App extends Component {
-  renderRoutes() {
-    let xHtml = null
-    xHtml = ROUTES.map((route, index) => {
-      return <AdminLayoutRoute key={index} route={route} />
-    })
-
-    return xHtml;
+  renderAdminRoutes() {
+    let xhtml = null;
+    xhtml = ADMIN_ROUTES.map(route => {
+      return (
+        <AdminLayoutRoute
+          key={route.path}
+          path={route.path}
+          component={route.component}
+          exact={route.exact}
+          name={route.name}
+        />
+      );
+    });
+    return xhtml;
   }
 
   render() {
-    const { classes } = this.props
     return (
       <Provider store={store}>
         <BrowserRouter>
           <ThemeProvider theme={theme}>
-            <ToastContainer/>
-            <GlobalLoading/>
-            <CustomModal/>
-            {/* <Taskboard/> */}
-            <Router>
-              {this.renderRoutes()}
-            </Router>
+            <CssBaseline />
+            <ToastContainer />
+            <GlobalLoading />
+            <CustomModal />
+            {/* <Routes> */}
+              {this.renderAdminRoutes()}
+             {/* </Routes> */}
           </ThemeProvider>
         </BrowserRouter>
       </Provider>
