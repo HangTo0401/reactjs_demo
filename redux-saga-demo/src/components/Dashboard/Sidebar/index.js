5ca33f21dd24a3f8a8b51ca6a683c86609889227
HangTo <tominhhang1996@gmail.com>
2022-04-23 21:36:31 +0700

    Implement Drawer for Sidebar

--- redux-saga-demo/src/components/Dashboard/Sidebar/index.js
+++ redux-saga-demo/src/components/Dashboard/Sidebar/index.js
@@ -4,6 +4,40 @@ import styles from './styles.js';
 import { connect } from 'react-redux';
 import { compose } from 'redux';
 class Sidebar extends Component {
+  toggleDrawer = value => {
+    const { onToggleSidebar } = this.props;
+    if (onToggleSidebar) {
+      onToggleSidebar(value);
+    }
+  };
+
+  renderList() {
+    const { classes } = this.props;
+    let xhtml = null;
+    xhtml = (
+      <div className={classes.list}>
+        <List component="div">
+          {ADMIN_ROUTES.map(item => {
+            return (
+              <NavLink
+                key={item.path}
+                to={item.path}
+                exact={item.exact}
+                className={classes.menuLink}
+                activeClassName={classes.menuLinkActive}
+              >
+                <ListItem className={classes.menuItem} button>
+                  {item.name}
+                </ListItem>
+              </NavLink>
+            );
+          })}
+        </List>
+      </div>
+    );
+    return xhtml;
+  }
+  
   render() {
     return (
       <div>Sidebar</div>
